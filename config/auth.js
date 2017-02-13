505e203c2fbab41d23a3aa1ce950f82321d4b14d
edeere <ecdeere@gmail.com>
2017-02-13 01:14:06 -0600

    Auth Setup

---
+++ config/auth.js
@@ -0,0 +1,17 @@
+module.exports = {
+    facebookAuth: {
+        clientID: '1081756871935589',
+        clientSecret: 'aa7d5871608e40426c80fbfb6d3b92a6',
+        callbackURL: 'http://localhost:3000/auth/facebook/callback',
+    },
+    twitterAuth: {
+        consumerKey: 'YOUR-TWITTER-CONSUMER-KEY',
+        consumerSecret: 'YOUR-TWITTER-CONSUMER-SECRET',
+        callbackURL: 'http://localhost:3000/auth/twitter/callback',
+    },
+    googleAuth: {
+        clientID: '254191798770-60oppcuam0fh6c101c5evjjgcuku9904.apps.googleusercontent.com',
+        clientSecret: 'RW9GckAEC_7LjT-B_9Wqtkur',
+        callbackURL: 'http://localhost:3000/auth/google/callback',
+    }
+};
